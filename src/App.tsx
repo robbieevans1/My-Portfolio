619d29a9b1d5a6f215d76c3ee4101fd58feffbb6
robcabobb <robcabobb@gmail.com>
2026-06-06 22:36:22 -0400

    added content to Hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 
 function App() {
 	return (
 		<main>
 			<Hero />
-			<About />
+			{/* <About />
 			<Projects />
-			<Contact />
+			<Contact /> */}
 		</main>
 	);
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,19 @@
-
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className="hero">
+			<p className="eyebrow">Hello, I'm Robert</p>
+			<h1>Software Engineer</h1>
+			<p className="hero-text">
+				I recently graduated with a degree in computer science! I enjoy building
+				applications and learning new things.
+			</p>
 
-export default Hero
+			<div className="hero-buttons">
+        <a href="#projects">View Projects</a>
+        <a href="contact" className="secondary">Contact Me</a>
+      </div>
+		</section>
+	);
+};
+
+export default Hero;
